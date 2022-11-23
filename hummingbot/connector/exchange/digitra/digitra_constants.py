68ae0b3c9bfb4952c70f40234dde011af91f0bc3
Reuel Ramos Ribeiro <rulrok@gmail.com>
2022-11-23 12:36:09 -0300

    (feat) Initial base template code

---
+++ hummingbot/connector/exchange/digitra/digitra_constants.py
@@ -0,0 +1,12 @@
+DEFAULT_DOMAIN = "com"
+
+REST_URL = "https://trade.api.digitra.{}"
+
+PUBLIC_API_VERSION = "v1"
+PRIVATE_API_VERSION = "v1"
+
+# Rate limit portion
+
+RATE_LIMITS = [
+    # TODO Add when necessary
+]
