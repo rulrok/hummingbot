4cb405e66c8a821b8f072754519a05c3d6522f7e
Reuel Ramos Ribeiro <rulrok@gmail.com>
2022-12-05 10:59:23 -0300

    (fix) Configure testnet and subdomain properly

--- hummingbot/connector/exchange/digitra/digitra_constants.py
+++ hummingbot/connector/exchange/digitra/digitra_constants.py
@@ -1,14 +1,49 @@
-DEFAULT_DOMAIN = "com"
+from hummingbot.core.api_throttler.data_types import LinkedLimitWeightPair, RateLimit
 
-REST_URL = "https://trade.api.digitra.{}/"
+DEFAULT_DOMAIN = "api"
+
+REST_SUBDOM_TRADE = "trade"
+REST_SUBDOM_BALANCE = "balance"
+
+REST_URL = "https://{}.{}.digitra.com/"
 
 PUBLIC_API_VERSION = "v1"
 PRIVATE_API_VERSION = "v1"
 
+# Public endpoints
 HEALTH_CHECK_URL = "/health-check"
+API_MARKETS = "/markets/{}"
+
+# Private endpoints
+API_BALANCES = "/balances"
+
+ENDPOINT_SUBDOM_MAP = {
+    # Trade
+    HEALTH_CHECK_URL: REST_SUBDOM_TRADE,
+    API_MARKETS: REST_SUBDOM_TRADE,
+
+    # Balance
+    API_BALANCES: REST_SUBDOM_BALANCE
+}
 
 # Rate limit portion
 
+HTTP_ENDPOINTS_LIMIT_ID = "AllHTTP"
+MAX_REQUESTS = 1500
+
+MINUTE = 60
+
 RATE_LIMITS = [
-    # TODO Add when necessary
+    # REST API Pool
+    RateLimit(limit_id=HTTP_ENDPOINTS_LIMIT_ID, limit=MAX_REQUESTS, time_interval=MINUTE),
+
+    # REST Endpoints
+    RateLimit(limit_id=HEALTH_CHECK_URL, limit=MAX_REQUESTS, time_interval=MINUTE,
+              linked_limits=[LinkedLimitWeightPair(HTTP_ENDPOINTS_LIMIT_ID)]),
+
+    RateLimit(limit_id=API_MARKETS.format("")[:-1], limit=MAX_REQUESTS, time_interval=MINUTE,
+              linked_limits=[LinkedLimitWeightPair(HTTP_ENDPOINTS_LIMIT_ID)]),
+
+    RateLimit(limit_id=API_BALANCES, limit=MAX_REQUESTS, time_interval=MINUTE,
+              linked_limits=[LinkedLimitWeightPair(HTTP_ENDPOINTS_LIMIT_ID)])
 ]
--- hummingbot/connector/exchange/digitra/digitra_web_utils.py
+++ hummingbot/connector/exchange/digitra/digitra_web_utils.py
@@ -1,9 +1,63 @@
+from datetime import datetime, timedelta, timezone
+from typing import Callable, Optional
+
 import hummingbot.connector.exchange.digitra.digitra_constants as CONSTANTS
+from hummingbot.connector.time_synchronizer import TimeSynchronizer
+from hummingbot.connector.utils import TimeSynchronizerRESTPreProcessor
+from hummingbot.core.api_throttler.async_throttler import AsyncThrottler
+from hummingbot.core.web_assistant.auth import AuthBase
+from hummingbot.core.web_assistant.web_assistants_factory import WebAssistantsFactory
 
 
 def public_rest_url(path_url: str, domain: str = CONSTANTS.DEFAULT_DOMAIN) -> str:
-    return CONSTANTS.REST_URL.format(domain) + CONSTANTS.PUBLIC_API_VERSION + path_url
+    subdomain = next((end for end in CONSTANTS.ENDPOINT_SUBDOM_MAP if path_url.__contains__(end)),
+                     CONSTANTS.REST_SUBDOM_TRADE)
+    return CONSTANTS.REST_URL.format(subdomain, domain) + CONSTANTS.PUBLIC_API_VERSION + path_url
 
 
 def private_rest_url(path_url: str, domain: str = CONSTANTS.DEFAULT_DOMAIN) -> str:
-    return CONSTANTS.REST_URL.format(domain) + CONSTANTS.PRIVATE_API_VERSION + path_url
+    subdomain = next((end for end in CONSTANTS.ENDPOINT_SUBDOM_MAP if path_url.__contains__(end)),
+                     CONSTANTS.REST_SUBDOM_TRADE)
+    return CONSTANTS.REST_URL.format(subdomain, domain) + CONSTANTS.PRIVATE_API_VERSION + path_url
+
+
+def build_api_factory(
+        throttler: Optional[AsyncThrottler] = None,
+        time_synchronizer: Optional[TimeSynchronizer] = None,
+        domain: str = CONSTANTS.DEFAULT_DOMAIN,
+        time_provider: Optional[Callable] = None,
+        auth: Optional[AuthBase] = None, ) -> WebAssistantsFactory:
+    time_synchronizer = time_synchronizer or TimeSynchronizer()
+    time_provider = time_provider or (lambda: get_current_server_time(
+        throttler=throttler,
+        domain=domain,
+    ))
+    throttler = throttler or create_throttler()
+    api_factory = WebAssistantsFactory(
+        throttler=throttler,
+        auth=auth,
+        rest_pre_processors=[
+            TimeSynchronizerRESTPreProcessor(synchronizer=time_synchronizer, time_provider=time_provider),
+        ])
+    return api_factory
+
+
+def build_api_factory_without_time_synchronizer_pre_processor(throttler: AsyncThrottler) -> WebAssistantsFactory:
+    api_factory = WebAssistantsFactory(throttler=throttler)
+    return api_factory
+
+
+def create_throttler() -> AsyncThrottler:
+    return AsyncThrottler(CONSTANTS.RATE_LIMITS)
+
+
+async def get_current_server_time(
+        throttler: Optional[AsyncThrottler] = None,
+        domain: str = CONSTANTS.DEFAULT_DOMAIN,
+) -> float:
+    # throttler = throttler or create_throttler()
+    # api_factory = build_api_factory_without_time_synchronizer_pre_processor(throttler=throttler)
+
+    timezone_offset = -3.0
+    tzinfo = timezone(timedelta(hours=timezone_offset))
+    return datetime.now(tzinfo).timestamp()
