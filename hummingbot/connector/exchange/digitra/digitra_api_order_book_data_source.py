0c49b8910083863a5da03d9fc1c196db690fb79c
Reuel Ramos Ribeiro <rulrok@gmail.com>
2022-12-08 15:35:00 -0300

    (fix) Undo wrong interpretation of tickers as trades

--- hummingbot/connector/exchange/digitra/digitra_api_order_book_data_source.py
+++ hummingbot/connector/exchange/digitra/digitra_api_order_book_data_source.py
@@ -8,7 +8,7 @@ from hummingbot.connector.exchange.digitra import digitra_web_utils as web_utils
 from hummingbot.connector.exchange.digitra.digitra_order_book import DigitraOrderBook
 from hummingbot.connector.time_synchronizer import TimeSynchronizer
 from hummingbot.core.api_throttler.async_throttler import AsyncThrottler
-from hummingbot.core.data_type.order_book_message import OrderBookMessage, OrderBookMessageType
+from hummingbot.core.data_type.order_book_message import OrderBookMessage
 from hummingbot.core.data_type.order_book_tracker_data_source import OrderBookTrackerDataSource
 from hummingbot.core.web_assistant.connections.data_types import RESTMethod, WSJSONRequest
 from hummingbot.core.web_assistant.web_assistants_factory import WebAssistantsFactory
@@ -81,27 +81,29 @@ class DigitraAPIOrderBookDataSource(OrderBookTrackerDataSource):
         )
 
     async def _parse_trade_message(self, raw_message: Dict[str, Any], message_queue: asyncio.Queue):
-        trade_data: Dict[str, Any] = raw_message["data"]
-        trade_timestamp: int = int(parser.isoparse(trade_data["time"]).timestamp())
-        trading_pair = await self._connector.trading_pair_associated_to_exchange_symbol(symbol=trade_data["market"])
+        # trade_data: Dict[str, Any] = raw_message["data"]
+        # trade_timestamp: int = int(parser.isoparse(trade_data["time"]).timestamp())
+        # trading_pair = await self._connector.trading_pair_associated_to_exchange_symbol(symbol=trade_data["market"])
 
-        message_content = {
-            "trading_pair": trading_pair,
-            # TODO
-            # "trade_type": (float(TradeType.SELL.value)
-            #                if trade_data["side"] == "sell"
-            #                else float(TradeType.BUY.value)),
-            "trade_id": trade_timestamp,
-            "update_id": trade_timestamp,
-            "price": trade_data["last"],
-            # "amount": trade_data["x"], TODO
-        }
-        trade_message: Optional[OrderBookMessage] = OrderBookMessage(
-            message_type=OrderBookMessageType.TRADE,
-            content=message_content,
-            timestamp=trade_timestamp)
+        # message_content = {
+        #     "trading_pair": trading_pair,
+        #     # TODO
+        #     # "trade_type": (float(TradeType.SELL.value)
+        #     #                if trade_data["side"] == "sell"
+        #     #                else float(TradeType.BUY.value)),
+        #     "trade_id": trade_timestamp,
+        #     "update_id": trade_timestamp,
+        #     "price": trade_data["last"],
+        #     # "amount": trade_data["x"], TODO
+        # }
+        # trade_message: Optional[OrderBookMessage] = OrderBookMessage(
+        #     message_type=OrderBookMessageType.TRADE,
+        #     content=message_content,
+        #     timestamp=trade_timestamp)
+        #
+        # message_queue.put_nowait(trade_message)
 
-        message_queue.put_nowait(trade_message)
+        pass
 
     async def _parse_order_book_diff_message(self, raw_message: Dict[str, Any], message_queue: asyncio.Queue):
         pass
@@ -138,7 +140,7 @@ class DigitraAPIOrderBookDataSource(OrderBookTrackerDataSource):
 
         if t == "update":
             if channel == "ticker":
-                return self._trade_messages_queue_key
+                return "ticker"
             if channel == "orderbook":
                 return self._snapshot_messages_queue_key  # Digitra sends the whole book on each update as of today
 
@@ -161,12 +163,14 @@ class DigitraAPIOrderBookDataSource(OrderBookTrackerDataSource):
         try:
             for trading_pair in self._trading_pairs:
                 symbol = await self._connector.exchange_symbol_associated_to_pair(trading_pair=trading_pair)
-                trade_payload = {
-                    "op": "subscribe",
-                    "channel": "ticker",
-                    "market": symbol,
-                }
-                subscribe_trade_request: WSJSONRequest = WSJSONRequest(payload=trade_payload)
+
+                # ticker_payload = {
+                #     "op": "subscribe",
+                #     "channel": "ticker",
+                #     "market": symbol,
+                # }
+                # subscribe_ticker_request: WSJSONRequest = WSJSONRequest(payload=ticker_payload)
+                # await ws.send(subscribe_ticker_request)
 
                 depth_payload = {
                     "op": "subscribe",
@@ -175,7 +179,6 @@ class DigitraAPIOrderBookDataSource(OrderBookTrackerDataSource):
                 }
                 subscribe_orderbook_request: WSJSONRequest = WSJSONRequest(payload=depth_payload)
 
-                await ws.send(subscribe_trade_request)
                 await ws.send(subscribe_orderbook_request)
 
                 self.logger().info(f"Subscribed to public order book and trade channels of {trading_pair}...")
